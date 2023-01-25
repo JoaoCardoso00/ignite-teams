c1768e47793794bca12e1c354346e9c279df78a2
JoaoCardoso00 <jvcq13@gmail.com>
2023-01-25 03:02:00 -0300

    added listEmpty component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,25 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galerinha do mal 😈"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
-      <GroupCard title="Turminha do mal 😈" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={<ListEmpty message="Nenhuma turma encontrada" />}
+      />
     </Container>
   );
 }
